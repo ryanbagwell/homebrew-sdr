60c7ef39283b1dfad3b912631400d057f8505175
Ryan Bagwell <ryan@ryanbagwell.com>
2022-08-24 08:59:09 -0400

    fix url source'

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -1,8 +1,8 @@
 class GrOsmosdr < Formula
   desc "GNU Radio hardware support for OsmoSDR and other devices"
   homepage "http://sdr.osmocom.org/trac/wiki/GrOsmoSDR"
-  url "http://git.osmocom.org/gr-osmosdr/snapshot/gr-osmosdr-0.1.4.tar.xz"
-  sha256 "1945d0d98fd4b600cb082970267ec2041528f13150422419cbd7febe2b622721"
+  url "https://gitea.osmocom.org/sdr/gr-osmosdr/archive/v0.1.4.tar.gz"
+  sha256 "ebd558380529a849d2d2807f01e56690e444847d2ab3de2829a97db5171aca5e"
   head "git://git.osmocom.org/gr-osmosdr"
 
   resource "Markdown" do
